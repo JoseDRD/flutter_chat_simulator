5b07b703acc26ee17b2ca20cb9dc2d0a73a84c72
José Del Rio <jose.chilwe@gmail.com>
2023-12-29 00:17:44 -0300

    avanze eventos

--- lib/src/widgets/text_field.dart
+++ lib/src/widgets/text_field.dart
@@ -6,6 +6,8 @@ class MiTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final texto_actual = TextEditingController();
+
     final colores = Theme.of(context).colorScheme;
 
     final borde = UnderlineInputBorder(
@@ -16,16 +18,25 @@ class MiTextField extends StatelessWidget {
     final decoracion = InputDecoration(
         enabledBorder: borde,
         focusedBorder: borde,
+        hintText: 'Escribe algo',
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
-          onPressed: (){},
+          onPressed: (){
+            texto_actual.clear();
+          },
         )
       );
+
+
 
     return TextFormField(
       decoration: decoracion,
       
+      controller: texto_actual,
+      onFieldSubmitted: (textoCompleto){
+        texto_actual.clear();
+      },
       onChanged: (texto){
         print(texto);
       },
